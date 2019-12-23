7a626d078ad0581be6dc77be2556eab5348f5323
Dougllas Sousa <dougllasfps@gmail.com>
2019-12-23 17:21:23 -0300

    aula 1 criando primeiro componente

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function ComponenteFuncional(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>Hello</h1>
+  )
+}
+
+class App extends React.Component {
+  render(){
+    return (
+      <h1>Hello from App</h1>
+    )
+  }
 }
 
 export default App;
